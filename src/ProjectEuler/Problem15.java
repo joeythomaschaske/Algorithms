a33cafb0292e4df6d52758f4caf1ff885dbf6999
Joseph Thomaschaske <josephthomaschaske@Josephs-MacBook-Pro.local>
2016-05-28 20:41:39 -0500

    a nice fast solution

--- src/ProjectEuler/Problem15.java
+++ src/ProjectEuler/Problem15.java
@@ -11,27 +11,49 @@ public class Problem15 {
     {
         int x = 20;
         int y = 20;
-        BigInteger numPaths = BigInteger.valueOf(0);
-        System.out.println("Answer: " + numPaths(x, y, numPaths));
+        ArrayList<ArrayList<BigInteger>> grid = new ArrayList<>();
+        for(int i = 0; i <= 20; ++i)
+        {
+            ArrayList<BigInteger> column = new ArrayList<>();
+            for(int j = 0; j <= 20; ++j)
+            {
+                column.add(BigInteger.valueOf(0));
+            }
+            grid.add(column);
+        }
+        System.out.println("Answer: " + numberPaths(grid));
+        String gridString = "";
+        for(int i = 0; i <= 20; ++i)
+        {
+            for(int j = 0; j <= 20; ++j)
+            {
+                gridString += grid.get(j).get(i)  + " ";
+            }
+            gridString += "\n";
+        }
+        System.out.println("Grid: \n" + gridString);
     }
 
-    private static BigInteger numPaths(int x, int y, BigInteger pathsFound)
+    private static BigInteger numberPaths(ArrayList<ArrayList<BigInteger>> grid)
     {
-        if(x == 0 && y == 0)
+        for(int i = 0; i <= 20; ++i)
         {
-            pathsFound = pathsFound.add(BigInteger.valueOf(1));
-        }
-        else
-        {
-            if(x > 0)
+            for(int j = 0; j <= 20; ++j)
             {
-                pathsFound = numPaths(x - 1, y, pathsFound);
-            }
-            if(y > 0)
-            {
-                pathsFound = numPaths(x, y - 1, pathsFound);
+                int x = j;
+                int y = i;
+                if(x - 1 < 0 || y - 1 < 0)
+                {
+                    grid.get(x).set(y, BigInteger.ONE);
+                }
+                else
+                {
+                    BigInteger topVal = grid.get(x - 1).get(y);
+                    BigInteger leftVal = grid.get(x).get(y - 1);
+                    grid.get(x).set(y, topVal.add(leftVal));
+                }
             }
         }
-        return pathsFound;
+        return grid.get(20).get(20);
     }
 }
